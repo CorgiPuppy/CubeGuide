b1dbcc636fce5f7c82770f4bd981454ef3583dda
Corgi <andrey.zolotukhin2017@mail.ru>
2025-11-07 11:33:00 +0300

    feat: add an object with all questions.

--- app/src/main/java/com/example/cubeguide/Questions.kt
+++ app/src/main/java/com/example/cubeguide/Questions.kt
@@ -7,3 +7,16 @@ data class Question(
     val correctAnswer: String,
     val type: String
 )
+
+object Questions {
+    val questions = listOf(
+        Question(1, "Как называется данная головоломка?", R.drawable.megaminx, "Мегаминкс", "image"),
+        Question(2, "Что изображено на рисунке?", R.drawable.mirror, "Зеркальный куб", "image"),
+
+        Question(3, "Сколько граней у классического кубика Рубика?", null, "6", "text"),
+        Question(4, "Как называется самый популярный метод сборки?", null, "Метод Фридрих", "text"),
+
+        Question(5, "Назовите виды головоломок", null, "Кубик Рубика, Мегаминкс, Зеркальный куб", "text"),
+        Question(6, "Основные этапы сборки", null, "Крест, Углы, Второй слой, ОЛЛ, ПЛЛ", "text")
+    )
+}
