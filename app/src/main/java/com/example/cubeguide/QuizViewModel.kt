a79cd2ae5f60612fb5a0f57aae526627597adf09
Corgi <andrey.zolotukhin2017@mail.ru>
2025-11-28 13:42:45 +0300

    feat: add some questions.

---
+++ app/src/main/java/com/example/cubeguide/QuizViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.cubeguide
+
+import androidx.lifecycle.ViewModel
+
+class QuizViewModel : ViewModel() {
+    private val questions = listOf(
+        Question(
+            text = "Какие из этих головоломок имеют форму куба?",
+            type = QuestionType.CHECKBOX,
+            answers = listOf("Megaminx", "2x2", "Mirror cube", "Pyraminx"),
+            correctAnswers = listOf("2x2", "Mirror cube")
+        ),
+
+        Question(
+            text = "Какие методы используются для скоростной сборки?",
+            type = QuestionType.CHECKBOX,
+            answers = listOf("CFOP", "Roux", "Послойный", "Интуитивный"),
+            correctAnswers = listOf("CFOP", "Roux")
+        ),
+
+        Question(
+            text = "Сколько граней у классического кубика Рубика? (число)",
+            type = QuestionType.FREE_INPUT,
+            answers = emptyList(),
+            correctAnswers = listOf("6", "шесть")
+        ),
+
+        Question(
+            text = "Как называется эта головоломка?",
+            type = QuestionType.FREE_INPUT,
+            answers = emptyList(),
+            correctAnswers = listOf("megaminx", "мегаминкс")
+        ),
+
+        Question(
+            text = "Как называется у этой головоломка? (число)",
+            type = QuestionType.FREE_INPUT,
+            answers = emptyList(),
+            correctAnswers = listOf("megaminx", "мегаминкс"),
+            ImageResId = R.drawable.four_on_four
+        )
+    )
+
+    var currentIndex = 0
+    var score = 0
+    var isGameFinished = false
+}
