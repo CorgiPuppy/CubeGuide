5cb005966c917ac64600d34d3b92415ebdc77ebf
Corgi <andrey.zolotukhin2017@mail.ru>
2025-12-12 06:20:18 +0300

    feat: replace strings on string resources.

--- app/src/main/java/com/example/cubeguide/QuizViewModel.kt
+++ app/src/main/java/com/example/cubeguide/QuizViewModel.kt
@@ -1,43 +1,90 @@
 package com.example.cubeguide
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 
-class QuizViewModel : ViewModel() {
+class QuizViewModel(application: Application) : AndroidViewModel(application) {
+    private fun getString(resId: Int): String {
+        return getApplication<Application>().resources.getString(resId)
+    }
+
     private val questions = listOf(
         Question(
-            text = "Какие из этих головоломок имеют форму куба?",
+            text = getString(R.string.q1_text),
             type = QuestionType.CHECKBOX,
-            answers = listOf("Megaminx", "2x2", "Mirror cube", "Pyraminx"),
-            correctAnswers = listOf("2x2", "Mirror cube")
+            answers = listOf(
+                getString(R.string.q1_a1),
+                getString(R.string.q1_a2),
+                getString(R.string.q1_a3),
+                getString(R.string.q1_a4),
+            ),
+            correctAnswers = listOf(
+                getString(R.string.q1_a2),
+                getString(R.string.q1_a3)
+            )
         ),
 
         Question(
-            text = "Какие методы используются для скоростной сборки?",
+            text = getString(R.string.q2_text),
             type = QuestionType.CHECKBOX,
-            answers = listOf("CFOP", "Roux", "Послойный", "Интуитивный"),
-            correctAnswers = listOf("CFOP", "Roux")
+            answers = listOf(
+                getString(R.string.q2_a1),
+                getString(R.string.q2_a2),
+                getString(R.string.q2_a3),
+                getString(R.string.q2_a4),
+            ),
+            correctAnswers = listOf(
+                getString(R.string.q2_a1),
+                getString(R.string.q2_a2)
+            )
         ),
 
         Question(
-            text = "Сколько граней у классического кубика Рубика? (число)",
+            text = getString(R.string.q3_text),
             type = QuestionType.FREE_INPUT,
             answers = emptyList(),
-            correctAnswers = listOf("6", "шесть")
+            correctAnswers = listOf(
+                getString(R.string.q3_a1),
+                getString(R.string.q3_a2),
+                getString(R.string.q3_a3),
+            )
         ),
 
         Question(
-            text = "Как называется эта головоломка?",
+            text = getString(R.string.q4_text),
             type = QuestionType.FREE_INPUT,
             answers = emptyList(),
-            correctAnswers = listOf("megaminx", "мегаминкс")
+            correctAnswers = listOf(
+                getString(R.string.q4_a1),
+                getString(R.string.q4_a2),
+            ),
         ),
 
         Question(
-            text = "Как называется у этой головоломка? (число)",
+            text = getString(R.string.q5_text),
             type = QuestionType.FREE_INPUT,
             answers = emptyList(),
-            correctAnswers = listOf("megaminx", "мегаминкс"),
+            correctAnswers = listOf(
+                getString(R.string.q5_a1),
+                getString(R.string.q5_a2),
+                getString(R.string.q5_a3),
+                getString(R.string.q5_a4),
+                getString(R.string.q5_a5),
+                getString(R.string.q5_a6),
+                getString(R.string.q5_a7),
+            ),
             imageResId = R.drawable.four_on_four
+        ),
+
+        Question(
+            text = getString(R.string.q6_text),
+            type = QuestionType.FREE_INPUT,
+            answers = emptyList(),
+            correctAnswers = listOf(
+                getString(R.string.q6_a1),
+                getString(R.string.q6_a2),
+            ),
+            imageResId = R.drawable.megaminx
         )
     )
 
@@ -61,8 +108,11 @@ class QuizViewModel : ViewModel() {
             if (normalizedUserAnswers == normalizedCorrectAnswers)
                 score++
         } else
-            if (userAnswers.isNotEmpty() && normalizedCorrectAnswers.contains(normalizedUserAnswers[0]))
-                score++
+            if (userAnswers.isNotEmpty()) {
+                val userAnswer = userAnswers[0].trim().lowercase()
+                if (normalizedCorrectAnswers.contains(userAnswer))
+                    score++
+            }
     }
 
     fun moveToNext() {
