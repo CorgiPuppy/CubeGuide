f1a238df19d3fca4faf21936c4d4cc1f2ab22b0c
Corgi <andrey.zolotukhin2017@mail.ru>
2025-12-12 13:14:29 +0300

    feat: add lists for a cart and favourites and checking for duplicates.

--- app/src/main/java/com/example/cubeguide/ShopViewModel.kt
+++ app/src/main/java/com/example/cubeguide/ShopViewModel.kt
@@ -14,16 +14,25 @@ class ShopViewModel : ViewModel() {
         )
     )
 
+    val cartList = mutableListOf<Product>()
+    val favList = mutableListOf<Product>()
+
     val cartCount = MutableLiveData(0)
     val favCount = MutableLiveData(0)
 
     var selectedProduct: Product? = null
 
     fun addToCart() {
-        cartCount.value = (cartCount.value ?: 0) + 1
+        if (selectedProduct != null && !cartList.contains(selectedProduct)) {
+            cartList.add(selectedProduct!!)
+            cartCount.value = cartList.size
+        }
     }
 
     fun addToFav() {
-        favCount.value = (favCount.value ?: 0) + 1
+        if (selectedProduct != null && !favList.contains(selectedProduct)) {
+            favList.add(selectedProduct!!)
+            favCount.value = favList.size
+        }
     }
 }
