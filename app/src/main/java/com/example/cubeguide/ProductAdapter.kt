0ee903cd877868db310562a06e813a68237d7f16
Corgi <andrey.zolotukhin2017@mail.ru>
2025-12-12 08:15:47 +0300

    feat: create an adapter for RecyclerView.

---
+++ app/src/main/java/com/example/cubeguide/ProductAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.cubeguide
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.view.LayoutInflater
+
+abstract class ProductAdapter (
+    private val productList: List<Product>,
+    private val onClick: (Product) -> Unit
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val TYPE_PRODUCT = 0
+    private val TYPE_AD = 1
+
+    class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val image: ImageView = itemView.findViewById(R.id.iv_product)
+        val title: TextView = itemView.findViewById(R.id.tv_title)
+        val price: TextView = itemView.findViewById(R.id.tv_price)
+    }
+
+    class AdViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun getItemViewType(position: Int): Int {
+        return if ((position + 1) % 5 == 0) TYPE_AD else TYPE_PRODUCT
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == TYPE_AD) {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_ad, parent, false)
+            AdViewHolder(view)
+        } else {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_product, parent, false)
+            ProductViewHolder(view)
+        }
+    }
+
+    fun onBindViewHoder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position) == TYPE_PRODUCT) {
+            val productHolder = holder as ProductViewHolder
+            val product = productList[position]
+
+            productHolder.title.text = product.name
+            productHolder.price.text = "${product.price} ₽"
+            productHolder.image.setImageResource(product.imageResId)
+
+            holder.itemView.setOnClickListener {
+                onClick(product)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = productList.size
+}
